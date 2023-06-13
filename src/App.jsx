2240a2ce5c1b89ae3e0075ecbca24296b5054193
Abid Hussain <125920304+Abidhussainqazi@users.noreply.github.com>
2023-06-13 15:24:02 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calculator from './cal/Calculator';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Simple Calculator</h1>
+      <Calculator />
+    </div>
+  );
+};
+
+export default App;
